edb17acc37aa5c35fd747e2944912668d0044c61
skcbroo <skcbroo@gmail.com>
2025-07-17 18:08:25 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ export default function NavbarLayout({ children }) {
         <div className="min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600 text-white">
             {/* Navbar */}
             <nav className="bg-blue-500 text-white shadow-md px-6 py-4 flex items-center justify-between select-none">
+
                 <div className="flex items-center gap-2">
                     <img
                         src="/logo3.png"
