959c6858d00da91d818e83e17ec44ec86931d05d
skcbroo <skcbroo@gmail.com>
2025-07-18 13:09:02 -0300

    Update DetalhesCredito.jsx

--- src/pages/DetalhesCredito.jsx
+++ src/pages/DetalhesCredito.jsx
@@ -110,11 +110,12 @@ export default function DetalhesCredito() {
               <p><strong>{cotasDisponiveis > 0 ? '🟢' : '🔴'} Cotas disponíveis:</strong> {cotasDisponiveis}</p>
               <p><strong>💵 Preço por cota:</strong> {precoPorCota.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
 
-              {credito.cotasDoUsuario > 0 && (
+              {typeof credito.cotasDoUsuario === "number" && credito.cotasDoUsuario > 0 && (
               <p className="text-sm text-blue-600 font-medium mt-2">
               🔐 Você já possui <strong>{credito.cotasDoUsuario}</strong> cota(s) deste crédito.
-            </p>
-            )}
+              </p>
+              )}
+
           </div>
 
             {/* Aquisição */}
