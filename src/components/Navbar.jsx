c938af926addc83bdfa29e23cdf47ee42facfb75
skcbroo <skcbroo@gmail.com>
2025-07-18 15:36:01 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default function NavbarLayout({ children }) {
 
                 <div className="flex items-center gap-2">
                     <img
-                        src="/logo3.png"
+                        src="/midlej.png"
                         alt="Logo Midlej Capital"
                         className="h-10"
                         draggable="false"
