899a55cf1b47219c802c2fcd9a2ce0d7d2f467b3
skcbroo <skcbroo@gmail.com>
2025-07-21 18:12:58 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function NavbarLayout({ children }) {
     return (
         <div className="min-h-screen bg-[#2F5871] text-white">
             {/* Navbar branca */}
-            <nav className="bg-white text-black shadow-md px-6 py-4 flex items-center justify-between select-none">
+            <nav className="bg-white text-black shadow-md px-6 py-2 flex items-center justify-between select-none">
 
                 <div className="flex items-center gap-2">
                     <img
