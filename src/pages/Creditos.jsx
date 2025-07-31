9c780be4b80671b1d637dc211c140fb3be0e2234
skcbroo <skcbroo@gmail.com>
2025-07-31 17:01:32 -0300

    Update Creditos.jsx

--- src/pages/Creditos.jsx
+++ src/pages/Creditos.jsx
@@ -34,7 +34,7 @@ export default function Creditos() {
     return (
         <NavbarLayout>
             <h2 className="text-2xl font-bold text-center mb-6 select-none cursor-default">
-                Créditos Judiciais Disponíveis
+                Créditos Judiciais
             </h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto select-none cursor-default">
