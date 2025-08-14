149911be5b7671e9f1ce88d04580f4ca779c57bb
skcbroo <skcbroo@gmail.com>
2025-08-14 09:38:25 -0300

    Update MeusAtivos.jsx

--- src/pages/MeusAtivos.jsx
+++ src/pages/MeusAtivos.jsx
@@ -185,7 +185,7 @@ export default function MeusAtivos() {
   </h3>
   <div className="grid grid-cols-1 md:grid-cols-2 gap-8 text-sm text-gray-700 text-center">
     <div>
-      <p className="inline-block bg-blue-100 text-blue-800 px-3 py-1 text-xs font-semibold rounded-full uppercase tracking-wider mb-1">
+      <p className="inline-block bg-gray-100 text-gray-800 px-3 py-1 text-xs font-semibold rounded-full uppercase tracking-wider mb-1">
         Valor Aplicado
       </p>
       <p className="text-lg font-bold">
